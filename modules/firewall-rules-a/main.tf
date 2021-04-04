aabbaf7f87f1c6031293a56d760d417419dc1de7
sohnya <sonja.hiltunen@dialogue.co>
2021-04-04 19:52:50 -0400

    fix: ssh is not a protocol

--- modules/firewall-rules-a/main.tf
+++ modules/firewall-rules-a/main.tf
@@ -13,8 +13,32 @@ resource "google_compute_firewall" "requirement_4_1_3" {
     protocol = "icmp"
   }
 
-  source_tags = ["vm-aa"] # Everyone on the internet
+  source_tags = ["vm-aa"] 
   target_tags = ["vm-ba"]
+}
+
+resource "google_compute_firewall" "requirement_4_1_4" {
+  name    = "requirement-4-1-4"
+  network = "vpc-a"
+
+  deny {
+    protocol = "icmp"
+  }
+
+  source_tags = ["vm-ba"] 
+  target_tags = ["vm-aa"]
+}
+
+resource "google_compute_firewall" "requirement_4_2_1b" {
+  name    = "requirement-4-2-1b"
+  network = "vpc-a"
+
+  deny {
+    protocol = "icmp"
+  }
+
+  source_tags = ["vm-ab"] 
+  target_tags = ["vm-aa"]
 }
 
 resource "google_compute_firewall" "requirement_4_4_1" {
@@ -23,9 +47,72 @@ resource "google_compute_firewall" "requirement_4_4_1" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80"]
+    ports    = [80]
   }
 
   source_ranges = ["0.0.0.0/0"] # Everyone on the internet
+  target_tags = ["vm-ab"]
+}
+
+resource "google_compute_firewall" "requirement_4_4_3" {
+  name    = "requirement-4-4-3"
+  network = "vpc-a"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Everyone on the internet
+  target_tags = ["vm-ab"]
+}
+
+resource "google_compute_firewall" "requirement_4_4_4" {
+  name    = "requirement-4-4-4"
+  network = "vpc-a"
+
+  deny {
+    protocol = "tcp"
+    ports = [22]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Everyone on the internet
+  target_tags = ["vm-ab"]
+}
+
+resource "google_compute_firewall" "requirement_4_5_1" {
+  name    = "requirement-4-5-1"
+  network = "vpc-a"
+
+  deny {
+    protocol = "tcp"
+    ports = [80]
+  }
+
+  source_tags = ["vm-bb"] # Everyone on the internet
+  target_tags = ["vm-ab"]
+}
+
+resource "google_compute_firewall" "requirement_4_5_2" {
+  name    = "requirement-4-5-2"
+  network = "vpc-a"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_tags = ["vm-bb"] # Everyone on the internet
+  target_tags = ["vm-ab"]
+}
+
+resource "google_compute_firewall" "requirement_4_5_3" {
+  name    = "requirement-4-5-3"
+  network = "vpc-a"
+
+  allow {
+    protocol = "tcp"
+    ports = [22]
+  }
+
+  source_tags = ["vm-bb"] # Everyone on the internet
   target_tags = ["vm-ab"]
 }
